JulianEberius/SublimePythonIDE

commit 7ef49d13ebb70246fb01509707c36ba5c845d5f4

- fixed issue #36 (unicode object sent to compile() builtin)

sublime_python_linting.py
@@ -126,8 +126,11 @@ class PythonLintingListener(sublime_plugin.EventListener):
                 'pyflakes_ignore', view, default_value=[]),
         }
 
-        errors = proxy.check_syntax(view.substr(
-            sublime.Region(0, view.size())), lint_settings, filename)
+        code = view.substr(sublime.Region(0, view.size()))
+        # python's compile function does not take unicode strings if
+        # they contain "# coding: " statements
+        code = code.encode(view.encoding())
+        errors = proxy.check_syntax(code, lint_settings, filename)
         try:
             errors = pickle.loads(errors.data)
 

commit 65801a8c775b3780cba5b49d8975110f12dd2e79

- hotfix the unicode issue introduced in 7ef49d13eb by passing explicit encoding info to the server

server/linter.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-
-## Taken from SublimeLinter
 import sys
 import os
 import os.path
@@ -66,9 +64,9 @@ class PythonError(PythonLintError):
         )
 
 
-def pyflakes_check(code, filename, ignore=None):
+def pyflakes_check(code, encoding, filename, ignore=None):
     try:
-        tree = compile(code, filename, "exec", _ast.PyCF_ONLY_AST)
+        tree = compile(code.encode(encoding), filename, "exec", _ast.PyCF_ONLY_AST)
     except (SyntaxError, IndentationError) as value:
         msg = value.args[0]
 
@@ -163,7 +161,7 @@ def pep8_check(code, filename, ignore=None, max_line_length=pep8.MAX_LINE_LENGTH
     return messages
 
 
-def do_linting(lint_settings, code, filename):
+def do_linting(lint_settings, code, encoding, filename):
     errors = []
 
     if lint_settings.get("pep8", True):
@@ -180,6 +178,6 @@ def do_linting(lint_settings, code, filename):
     pyflakes_disabled = lint_settings.get('pyflakes_disabled', False)
 
     if not pyflakes_disabled:
-        errors.extend(pyflakes_check(code, filename, pyflakes_ignore))
+        errors.extend(pyflakes_check(code, encoding, filename, pyflakes_ignore))
 
     return errors

server/server.py
@@ -295,11 +295,11 @@ class LinterMixin(object):
     cases.
     """
 
-    def check_syntax(self, code, lint_settings, filename):
+    def check_syntax(self, code, encoding, lint_settings, filename):
         '''The linting mixin does not use the project_for machinery,
         but uses the linters directy.'''
         try:
-            codes = do_linting(lint_settings, code, filename)
+            codes = do_linting(lint_settings, code, encoding, filename)
         except Exception:
             import traceback
             sys.stderr.write(traceback.format_exc())

sublime_python_linting.py
@@ -129,10 +129,8 @@ class PythonLintingListener(sublime_plugin.EventListener):
         }
 
         code = view.substr(sublime.Region(0, view.size()))
-        # python's compile function does not take unicode strings if
-        # they contain "# coding: " statements
-        code = code.encode(view.encoding())
-        errors = proxy.check_syntax(code, lint_settings, filename)
+        encoding = view.encoding() or "utf-8"
+        errors = proxy.check_syntax(code, encoding, lint_settings, filename)
         try:
             errors = pickle.loads(errors.data)
 