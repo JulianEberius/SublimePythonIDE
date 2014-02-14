ab3d9e933e2154e047a5f6c6b40adead720514f3
Mikko Ohtamaa <mikko@localbitcoins.com>
2014-02-14 17:33:49 +0200

    Updated pyflakes to 0.7.3

--- lib/pyflakes/messages.py
+++ lib/pyflakes/messages.py
@@ -1,115 +1,119 @@
-# (c) 2005 Divmod, Inc.  See LICENSE file for details
+"""
+Provide the class Message and its subclasses.
+"""
 
 
 class Message(object):
     message = ''
     message_args = ()
 
-    def __init__(self, filename, lineno):
+    def __init__(self, filename, loc):
         self.filename = filename
-        self.lineno = lineno
+        self.lineno = loc.lineno
+        self.col = getattr(loc, 'col_offset', 0)
 
     def __str__(self):
-        return '%s:%s: %s' % (self.filename, self.lineno, self.message % self.message_args)
+        return '%s:%s: %s' % (self.filename, self.lineno,
+                              self.message % self.message_args)
 
 
 class UnusedImport(Message):
     message = '%r imported but unused'
 
-    def __init__(self, filename, lineno, name):
-        Message.__init__(self, filename, lineno)
+    def __init__(self, filename, loc, name):
+        Message.__init__(self, filename, loc)
         self.message_args = (name,)
-        self.message = UnusedImport.message
 
 
 class RedefinedWhileUnused(Message):
     message = 'redefinition of unused %r from line %r'
 
-    def __init__(self, filename, lineno, name, orig_lineno):
-        Message.__init__(self, filename, lineno)
-        self.message_args = (name, orig_lineno)
-        self.message = RedefinedWhileUnused.message
+    def __init__(self, filename, loc, name, orig_loc):
+        Message.__init__(self, filename, loc)
+        self.message_args = (name, orig_loc.lineno)
 
 
 class RedefinedInListComp(Message):
     message = 'list comprehension redefines %r from line %r'
 
-    def __init__(self, filename, lineno, name, orig_lineno):
-        Message.__init__(self, filename, lineno)
-        self.message_args = (name, orig_lineno)
-        self.message = RedefinedInListComp.message
+    def __init__(self, filename, loc, name, orig_loc):
+        Message.__init__(self, filename, loc)
+        self.message_args = (name, orig_loc.lineno)
 
 
 class ImportShadowedByLoopVar(Message):
     message = 'import %r from line %r shadowed by loop variable'
 
-    def __init__(self, filename, lineno, name, orig_lineno):
-        Message.__init__(self, filename, lineno)
-        self.message_args = (name, orig_lineno)
-        self.message = ImportShadowedByLoopVar.message
+    def __init__(self, filename, loc, name, orig_loc):
+        Message.__init__(self, filename, loc)
+        self.message_args = (name, orig_loc.lineno)
 
 
 class ImportStarUsed(Message):
     message = "'from %s import *' used; unable to detect undefined names"
 
-    def __init__(self, filename, lineno, modname):
-        Message.__init__(self, filename, lineno)
+    def __init__(self, filename, loc, modname):
+        Message.__init__(self, filename, loc)
         self.message_args = (modname,)
-        self.message = ImportStarUsed.message
 
 
 class UndefinedName(Message):
     message = 'undefined name %r'
 
-    def __init__(self, filename, lineno, name):
-        Message.__init__(self, filename, lineno)
+    def __init__(self, filename, loc, name):
+        Message.__init__(self, filename, loc)
         self.message_args = (name,)
-        self.message = UndefinedName.message
+
+
+class DoctestSyntaxError(Message):
+    message = 'syntax error in doctest'
+
+    def __init__(self, filename, loc, position=None):
+        Message.__init__(self, filename, loc)
+        if position:
+            (self.lineno, self.col) = position
+        self.message_args = ()
 
 
 class UndefinedExport(Message):
     message = 'undefined name %r in __all__'
 
-    def __init__(self, filename, lineno, name):
-        Message.__init__(self, filename, lineno)
+    def __init__(self, filename, loc, name):
+        Message.__init__(self, filename, loc)
         self.message_args = (name,)
-        self.message = UndefinedExport.message
 
 
 class UndefinedLocal(Message):
-    message = "local variable %r (defined in enclosing scope on line %r) referenced before assignment"
+    message = ('local variable %r (defined in enclosing scope on line %r) '
+               'referenced before assignment')
 
-    def __init__(self, filename, lineno, name, orig_lineno):
-        Message.__init__(self, filename, lineno)
-        self.message_args = (name, orig_lineno)
-        self.message = UndefinedLocal.message
+    def __init__(self, filename, loc, name, orig_loc):
+        Message.__init__(self, filename, loc)
+        self.message_args = (name, orig_loc.lineno)
 
 
 class DuplicateArgument(Message):
     message = 'duplicate argument %r in function definition'
 
-    def __init__(self, filename, lineno, name):
-        Message.__init__(self, filename, lineno)
+    def __init__(self, filename, loc, name):
+        Message.__init__(self, filename, loc)
         self.message_args = (name,)
-        self.message = DuplicateArgument.message
 
 
 class Redefined(Message):
     message = 'redefinition of %r from line %r'
 
-    def __init__(self, filename, lineno, name, orig_lineno):
-        Message.__init__(self, filename, lineno)
-        self.message_args = (name, orig_lineno)
-        self.message = Redefined.message
+    def __init__(self, filename, loc, name, orig_loc):
+        Message.__init__(self, filename, loc)
+        self.message_args = (name, orig_loc.lineno)
 
 
 class LateFutureImport(Message):
     message = 'future import(s) %r after other statements'
 
-    def __init__(self, filename, lineno, names):
-        Message.__init__(self, filename, lineno)
+    def __init__(self, filename, loc, names):
+        Message.__init__(self, filename, loc)
         self.message_args = (names,)
-        self.message = LateFutureImport.message
 
 
 class UnusedVariable(Message):
@@ -119,7 +123,6 @@ class UnusedVariable(Message):
     """
     message = 'local variable %r is assigned to but never used'
 
-    def __init__(self, filename, lineno, names):
-        Message.__init__(self, filename, lineno)
+    def __init__(self, filename, loc, names):
+        Message.__init__(self, filename, loc)
         self.message_args = (names,)
-        self.message = UnusedVariable.message
