482fc7cb2825a0153f954471fc58b276ba4c9bfc
Julian Eberius <julian@eberius-mail.de>
2014-03-15 16:00:49 +0100

    merged util.py with main module

--- sublime_python.py
+++ sublime_python.py
@@ -9,9 +9,6 @@ import sublime
 import sublime_plugin
 from queue import Queue
 
-from SublimePythonIDE.util import AsynchronousFileReader, DebugProcDummy
-
-
 # contains root paths for each view, see root_folder_for()
 ROOT_PATHS = {}
 # contains proxy objects for external Python processes, by interpreter used
@@ -341,6 +338,7 @@ def root_folder_for(view):
 
     return root_path
 
+'''Plugin Commands and EventListeners'''
 
 class PythonStopServerCommand(sublime_plugin.WindowCommand):
     '''stops the server this view is connected to. unused'''
@@ -513,3 +511,47 @@ class PythonGoBackCommand(sublime_plugin.WindowCommand):
             file_name, lineno = GOTO_STACK.pop()
             path = file_name + ":" + str(lineno)
             self.window.open_file(path, sublime.ENCODED_POSITION)
+
+
+'''Utilities'''
+
+
+class SimpleClearAndInsertCommand(sublime_plugin.TextCommand):
+    '''utility command class for writing into the documentation view'''
+    def run(self, edit, block=False, **kwargs):
+        doc = kwargs['insert_string']
+        r = sublime.Region(0, self.view.size())
+        self.view.erase(edit, r)
+        self.view.insert(edit, 0, doc)
+
+
+class AsynchronousFileReader(threading.Thread):
+    '''
+    Helper class to implement asynchronous reading of a file
+    in a separate thread. Pushes read lines on a queue to
+    be consumed in another thread.
+
+    Used for reading stderr output of the server.
+    '''
+
+    def __init__(self, name, fd, queue):
+        threading.Thread.__init__(self)
+        self.name = name
+        self._fd = fd
+        self._queue = queue
+
+    def run(self):
+        '''The body of the tread: read lines and put them on the queue.'''
+        for line in iter(self._fd.readline, ''):
+            if line:
+                self._queue.put("{0}: {1}".format(self.name, line))
+
+
+class DebugProcDummy(object):
+    """Used only for debugging, when the server process is started externally
+    """
+    def poll(*args):
+        return None
+
+    def terminate():
+        pass
